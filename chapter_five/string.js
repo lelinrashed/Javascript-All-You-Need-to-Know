eb92533888b755370660660ca8754e4191dd57e6
lelinrashed <lelin.rashed784@gmail.com>
2020-11-08 23:17:49 +0600

    Push after compleate chapter five

---
+++ chapter_five/string.js
@@ -0,0 +1,92 @@
+/**
+ * String
+ * 
+ *  ' ', " "
+ * 
+ */
+
+// 's', 't', 'r', 'i', 'n', 'g'
+
+
+// var str = 'Something'
+
+// var str2 = String('Something')
+
+/**
+ * Number to String
+ */
+
+// console.log(str2);
+
+// var n = 10
+
+// var str = String(n)
+
+// console.log(str);
+
+
+/**
+ * Escape Notation
+ */
+
+// var str = 'This is a \'String\''
+
+// console.log(str);
+
+/**
+ * String comparison
+ * Javascript uses lexicographic system to compare to string
+ */
+
+
+// var a = 'abc'
+// var b = 'bcd'
+
+// console.log(a === b);
+
+// console.log('a' > 'Z');
+
+// console.log('001' == 1);
+
+
+/**
+ * String built in method
+ */
+
+
+// var a = 'I am'
+// var b = 'rashed'
+
+// var c = a.concat(' ', b)
+
+// var d = c.substr(5, 3)
+
+// console.log(d);
+
+// console.log('     raeaef   afeafaf      '.trim())
+
+// console.log(c.split(' '));
+
+
+/**
+ * String length
+ */
+
+var str = 'Some'
+
+// var str2 = str.charAt(88)
+
+// console.log(typeof str2);
+
+// var length = 0
+
+// while(true){
+//     if(str.charAt(length) == '') {
+//         break
+//     } else {
+//         length++
+//     }
+// }
+
+console.log(str.length);
+
