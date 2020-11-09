6a8c449bb6c977c677f7e60c25a5aedf1b6e0245
lelinrashed <lelin.rashed784@gmail.com>
2020-11-09 12:13:29 +0600

    Push after compleate chapter seven

---
+++ chapter_seven/object.js
@@ -0,0 +1,77 @@
+// Object in javascript
+
+// var obj = {
+
+// }
+
+// obj.x = 10
+
+// console.log(typeof obj);
+
+// console.log(obj);
+
+
+// var point = {
+//     x: 20,
+//     y: 30
+// }
+
+// point.z = 40
+
+// console.log(point);
+
+// var obj = Object()
+
+// obj.a = 10
+
+// console.log(obj);
+
+
+var point = {
+
+    x: 10,
+    y: 20,
+    z: 30
+
+}
+
+
+// console.log(point.x);
+// console.log(point.z);
+
+// console.log(point.x + point.z);
+
+
+// console.log(point['x']);
+
+
+
+// var show = 'x'
+
+// console.log(point[show]);
+
+
+// point.x = 50
+// point.a = 60
+
+// console.log(point.x);
+// console.log(point.a);
+// console.log(point);
+
+
+// point['y'] = 100
+
+// var prop = 'z'
+// point[prop] = 55
+// console.log(point);
+
+
+point.a = undefined
+
+console.log(point);
+
+delete point.a
+
+console.log(point);
+
+
